47b022723190c9dfb724ac72d583995a5e38ef0b
Roman Horbovyi <rhorbovyi@gmail.com>
2021-11-06 00:45:49 +0200

    Changes

---
+++ userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+systemctl status docker
+git clone https://github.com/RomanHorbovyi/jenkins-in-docker.git
+cd jenkins-in-docker
+docker-compose up
